b979dab4889213e8687d0d6d7e89cefd6c09561f
Lachesis <lachesis@example.invalid>
2026-04-24 13:29:10 -0400

    Support writing presaved images

--- scenes/tagtinker_scene_target_actions.c
+++ scenes/tagtinker_scene_target_actions.c
@@ -70,6 +70,7 @@ void tagtinker_scene_target_actions_on_enter(void* ctx) {
 
     if(allow_graphics) {
         submenu_add_item(app->submenu, "Set Text", TagTinkerTargetPushText, target_actions_cb, app);
+        submenu_add_item(app->submenu, "Set Image", TagTinkerTargetPushSyncedImage, target_actions_cb, app);
     }
 
     submenu_add_item(app->submenu, "LED Test", TagTinkerTargetPingFlash, target_actions_cb, app);
@@ -94,6 +95,10 @@ bool tagtinker_scene_target_actions_on_event(void* ctx, SceneManagerEvent event)
     case TagTinkerTargetPushText:
         if(!tagtinker_target_supports_graphics(&app->targets[app->selected_target])) return true;
         scene_manager_next_scene(app->scene_manager, TagTinkerScenePresetList);
+        return true;
+    case TagTinkerTargetPushSyncedImage:
+        if(!tagtinker_target_supports_graphics(&app->targets[app->selected_target])) return true;
+        scene_manager_next_scene(app->scene_manager, TagTinkerSceneSyncedImageList);
         return true;
     case TagTinkerTargetPingFlash:
         {
